4e38fa979b4a08aeaa18e305111a5ba75ba83d6d
Kelvin <kells4real@gmail.com>
2021-09-12 20:49:13 +0100

    Updated the api URL

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const axiosInstance = axios.create({
 //   baseURL: 'https://api.divineword.xyz'
-    baseURL: 'http://3.82.94.95/'
+    baseURL: 'http://ec2-52-13-104-158.us-west-2.compute.amazonaws.com/'
     /* other custom settings */
 });
 
